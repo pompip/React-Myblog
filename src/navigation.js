b888c5f740ef949aa15f37626660cc351d28aef0
pompip <pompip@163.com>
2019-03-24 02:22:19 +0800

    fist

---
+++ src/navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Navigation extends React.Component{
+    constructor(){
+
+    }
+
+    render(){
+        return(
+            <nav></nav>
+        )
+    }
+}
+
+export default Navigation;
